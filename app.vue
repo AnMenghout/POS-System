19196723a9575bebe7983847b84b4035d4d28116
AnMenghout <120448908+AnMenghout@users.noreply.github.com>
2024-10-08 16:22:52 +0700

    update app.vue

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-extract-style>
+    <NuxtLayout>
+      <a-config-provider :theme="{
+        token: {
+          colorPrimary: '#005C94',
+          // fontFamily: 'Noto Sans',
+          colorInfo: '#005C94',
+          colorLink: '#005C94',
+          colorText: '#4B465C',
+        },
+        components: {
+          Form: {
+            colorTextHeading: '#4B465C',
+          },
+        },
+      }">
+        <NuxtPage />
+      </a-config-provider>
+    </NuxtLayout>
+  </a-extract-style>
+</template>
